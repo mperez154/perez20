e04a3eb8feb2de6a75ea31c1c17283b78e2cbe26
Student <Student@10.20.88.235>
2016-11-10 20:08:34 -0500

    Updated CarComparator & CarLists

--- src/perez20/src/perez20/CarComparator.java
+++ src/perez20/src/perez20/CarComparator.java
@@ -5,17 +5,14 @@ import java.util.Comparator;
 public class CarComparator implements Comparator<Car> {
 
 	public int compare(Car arg0, Car arg1) {
-		// TODO Auto-generated method stub
-		double car1 = arg0.price;
-		double car2 = arg1.price;
 		
-		if(car1 > car2)
+		if(arg0.getPrice() > arg1.getPrice())
 		{
 			return 1;
 		}
-		else if(car1 < car2)
+		else if(arg0.getPrice() < arg1.getPrice())
 		{
-			return 2;
+			return -1;
 		}
 		else
 			return 0;
--- src/perez20/src/perez20/CarLists.java
+++ src/perez20/src/perez20/CarLists.java
@@ -1,32 +1,64 @@
 package perez20;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class CarLists {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Car> myCars = new ArrayList<>();
-				
+		
+		//Adding cars to the ArrayList
 		myCars.add(new Car("Chevy", "Sonic", 11999.99));
 		myCars.add(new Car("Chevy", "Corvette", 75999.99));
 		myCars.add(new Car("Chevy", "Silverado", 29999.99));
+		myCars.add(new Car("Toyota", "Tundra", 36999.99));
+		myCars.add(new Car("Acura", "TL", 42999.99));
+		myCars.add(new Car("BMW", "3 Series", 45999.99));
 		
+		//Sorting cars by price using Collections & Car comparator	
+		Collections.sort(myCars, new CarComparator());
+		
+		//Display entire collections of cars (after sort)
+		System.out.println("Cars sorted by price...");
 		Iterator<Car> iterator = myCars.iterator();
 		while(iterator.hasNext()){
 			System.out.println(iterator.next().carToString());
 		}
 		
-		//See test comparator for help with finding the most expensive car (pages 774-777)
-		//Collections.max(myCars, CarComparator)); (page 776 has example)
-		//Use the collections class to sort by price, display most expensive
+		//Get the highest priced car
+		System.out.println("\n\nMost Expensive " + Collections.max(myCars, new CarComparator()).carToString() + "\n\n");
+			
+		//Creating a LinkedList from an Arary List
+		LinkedList<Car> linkedList = new LinkedList<>(myCars);
+		//Adding car to linked list at index 4
+		linkedList.add(4, new Car("Ford", "Mustang", 19999.99));
+		//Sorting the linked list
+		Collections.sort(linkedList, new CarComparator());
+		
+		//Creating a ListIterator and displaying cars (will display by price 
+		//if code placed after the Collections.sort method
+		ListIterator<Car> listCars = linkedList.listIterator();
+		while(listCars.hasNext())
+		{
+			System.out.println(listCars.next().carToString());
+		}
+		
+		//Remove the first and last cars from the linkedList
+		System.out.println("\n\n");
+		System.out.println(linkedList.removeLast().carToString());
+		System.out.println(linkedList.removeFirst().carToString());	
+		
+		//Reverse the order of the cars, can use ListIterator with hasPrevious()
+		Collections.reverseOrder(new CarComparator());
 
 	}
 
 }
-
-
 /*
 Class CarLists (main class)  code it in this order
 •	Create an ArrayList of at least six cars, not ordered in any way by price.
